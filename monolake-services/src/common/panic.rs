0364d9128a75986c847e961d88c258a697799a4b
ihc童鞋@提不起劲 <ihciah@gmail.com>
2024-05-21 11:44:35 +0800

    improve: custom CatchPanicService's error to enum which can convert to AnyError (#97)

--- monolake-services/src/common/panic.rs
+++ monolake-services/src/common/panic.rs
@@ -1,4 +1,9 @@
-use std::{any::Any, panic::AssertUnwindSafe};
+use std::{
+    any::Any,
+    error::Error,
+    fmt::{Debug, Display},
+    panic::AssertUnwindSafe,
+};
 
 use futures::FutureExt;
 use service_async::{
@@ -10,6 +15,39 @@ pub struct CatchPanicService<S> {
     inner: S,
 }
 
+#[derive(Clone)]
+pub enum CatchPanicError<E> {
+    Inner(E),
+    // to make it Sync, construct a String instead of Box<dyn Ayn + Send>
+    Panic(String),
+}
+
+impl<E> From<Box<dyn Any + Send>> for CatchPanicError<E> {
+    fn from(value: Box<dyn Any + Send>) -> Self {
+        CatchPanicError::Panic(format!("Panic: {value:?}"))
+    }
+}
+
+impl<E: Debug> Debug for CatchPanicError<E> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Inner(e) => write!(f, "Inner error: {e:?}"),
+            Self::Panic(e) => write!(f, "Panic error: {e}"),
+        }
+    }
+}
+
+impl<E: Display> Display for CatchPanicError<E> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Inner(e) => write!(f, "Inner error: {e}"),
+            Self::Panic(e) => write!(f, "Panic error: {e}"),
+        }
+    }
+}
+
+impl<E: Error> Error for CatchPanicError<E> {}
+
 /// `CatchPanicService` is designed to prevent a panic from causing
 /// the entire program to crash by converting the panic into an error.
 /// It's important to note that the user must ensure that the inner service
@@ -18,15 +56,15 @@ pub struct CatchPanicService<S> {
 impl<R, S> Service<R> for CatchPanicService<S>
 where
     S: Service<R>,
-    S::Error: From<Box<dyn Any + Send + 'static>>,
 {
     type Response = S::Response;
-    type Error = S::Error;
+    type Error = CatchPanicError<S::Error>;
 
     async fn call(&self, req: R) -> Result<Self::Response, Self::Error> {
         match AssertUnwindSafe(self.inner.call(req)).catch_unwind().await {
-            Ok(r) => r,
-            Err(e) => Err(e.into()),
+            Ok(Ok(r)) => Ok(r),
+            Ok(Err(e)) => Err(CatchPanicError::Inner(e)),
+            Err(e) => Err(CatchPanicError::from(e)),
         }
     }
 }
